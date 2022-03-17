902e5406be21a36bc8ae7ae3fedc5d2e0dca9179
awatersny <awatersny@gmail.com>
2022-03-17 12:15:34 -0400

    Render landing page

---
+++ src/StarShips/StarShips.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useImperativeHandle } from 'react';
+import { getAllStarShips } from '../services/sw-api';
+import { Link } from 'react-router-dom'
+
+const StarShips = (props) => {
+  return ( 
+    <>
+      <h2>All StarShips</h2>
+    </>
+  );
+}
+
+export default StarShips;
