694267c6310398ea3f6d4f26748ecd1e1bd820b2
awatersny <awatersny@gmail.com>
2022-03-17 12:43:09 -0400

    Display instances of starship component

--- src/StarShips/StarShips.jsx
+++ src/StarShips/StarShips.jsx
@@ -3,9 +3,23 @@ import { getAllStarShips } from '../services/sw-api';
 import { Link } from 'react-router-dom'
 
 const StarShips = (props) => {
+  const [starShips, setStarShips] = useState([]);
+
+  useEffect(()=>{
+    getAllStarShips()
+    .then(starShipData => setStarShips(starShipData.results))
+  });
+
+  console.log(starShips.map)
+
   return ( 
     <>
       <h2>All StarShips</h2>
+      {starShips.map(starShip => 
+        <div key={starShip.name}>
+          <h1>StarShip</h1>
+        </div>
+      )}
     </>
   );
 }
